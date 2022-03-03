bb3b862831a8eaab6e067c35e72e2d4aab3fb9c5
Victor Niño <ninomtz.victor@gmail.com>
2022-03-03 07:20:17 -0600

    feat: ejercicio foodAp

---
+++ foodApp/data.js
@@ -0,0 +1,79 @@
+export default [
+  {
+    nombre: "Acelga cruda",
+    cantidad: "2 tazas",
+    categoria: "verduras",
+    cover:
+      "http://static1.squarespace.com/static/51c273cee4b0d73628457d7c/t/5220c653e4b0ad8fc2b00476/1377879641207/Chard.jpg?format=original",
+    descripcion:
+      "La acelga es un miembro de la familia de remolachas y espinaca, pero típicamente se la agrupa con mostaza parda, la col berza y la col rizada. Tiene hojas grandes y anchas con un tallo crujiente. El tallo puede ser blanco, rosado vívido o rojo. Las hojas son más oscuras en los fines. Ambos las hojas y el tallo son comestibles",
+    imagenes: [
+      "https://5aldia.cl/wp-content/uploads/2018/03/acelga-1.jpg",
+      "https://www.hogarmania.com/archivos/201402/5318-nutricion-ainhoa-acelgas-xl-668x400x80xX.jpg",
+    ],
+  },
+  {
+    nombre: "Apio crudo",
+    cantidad: "1 1/2 taza",
+    categoria: "verduras",
+    cover:
+      "https://www.opinion.com.bo/media/opinion/images/2016/12/17/2016S10600.jpg",
+    descripcion: "",
+  },
+  {
+    nombre: "Arúgula cruda",
+    cantidad: "4 tazas",
+    categoria: "verduras",
+    cover:
+      "https://ingredienta.com/wp-content/uploads/2020/09/Comp-arugula-baby.jpg",
+    descripcion: "",
+  },
+  {
+    nombre: "Berenjena picada cocida",
+    cantidad: "1 taza",
+    categoria: "verduras",
+    cover:
+      "https://cdn.pixabay.com/photo/2016/12/26/17/28/spaghetti-1932466_960_720.jpg",
+    descripcion: "",
+  },
+  {
+    nombre: "Arándano fresco",
+    cantidad: "125 gramos",
+    categoria: "frutas",
+    cover:
+      "https://cdn.pixabay.com/photo/2016/12/26/17/28/spaghetti-1932466_960_720.jpg",
+    descripcion: "",
+  },
+  {
+    nombre: "Cereza",
+    cantidad: "20 piezas",
+    categoria: "frutas",
+    cover:
+      "https://cdn.pixabay.com/photo/2016/12/26/17/28/spaghetti-1932466_960_720.jpg",
+    descripcion: "",
+  },
+  {
+    nombre: "Ciruela roja o amarilla",
+    cantidad: "3 piezas",
+    categoria: "frutas",
+    cover:
+      "https://cdn.pixabay.com/photo/2016/12/26/17/28/spaghetti-1932466_960_720.jpg",
+    descripcion: "",
+  },
+  {
+    nombre: "Ciruela pasa deshuesada",
+    cantidad: "7 piezas",
+    categoria: "frutas",
+    cover:
+      "https://cdn.pixabay.com/photo/2016/12/26/17/28/spaghetti-1932466_960_720.jpg",
+    descripcion: "",
+  },
+  {
+    nombre: "Chabacano",
+    cantidad: "4 piezas",
+    categoria: "frutas",
+    cover:
+      "https://cdn.pixabay.com/photo/2016/12/26/17/28/spaghetti-1932466_960_720.jpg",
+    descripcion: "",
+  },
+];
---
+++ foodApp/foodApp.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <h1>Food App</h1>
+    <div>
+      <label for="search">Buscar</label>
+      <input v-model="search" type="text" id="search" />
+    </div>
+    <div class="food-wrapper">
+      <div
+        class="food--item"
+        v-for="(item, index) in foodFiltered"
+        :key="index"
+      >
+        <strong>{{ item.nombre }}</strong>
+        <br />
+        Porción recomendada: <em> {{ item.cantidad }}</em>
+        <br />
+        <code>{{ item.categoria }}</code>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import food from "./data";
+
+export default {
+  name: "FoodApp",
+  data() {
+    return {
+      food: food,
+      search: "",
+    };
+  },
+  computed: {
+    foodFiltered() {
+      return this.food.filter((item) => {
+        if (item.nombre.includes(this.search)) {
+          return item;
+        }
+      });
+    },
+  },
+};
+</script>
+
+<style>
+.food-wrapper {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  max-width: 100%;
+  padding: 10px;
+}
+.food--item {
+  margin: 5px;
+  padding: 10px;
+  background-color: aliceblue;
+  border-radius: 10px;
+}
+</style>
